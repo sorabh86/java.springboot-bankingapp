7404d0958307129a718d19092913406380c8a48c
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-09-03 18:50:54 +0530

    added signup page

--- src/main/java/com/userfront/controller/HomeController.java
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -5,8 +5,14 @@
  */
 package com.userfront.controller;
 
+import com.userfront.domain.User;
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *
@@ -23,5 +29,33 @@ public class HomeController {
     @RequestMapping("/index")
     public String index() {
         return "index";
+    }
+    
+    @RequestMapping(value="/signup", method=RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+        
+        model.addAttribute("user", user);
+        
+        return "signup";
+    }
+    
+    @RequestMapping(value="/signup", method=RequestMethod.POST)
+    public String signupPost(@ModelAttribute("user") User user, Model model) {
+        if(userService.checkUserExists(user.getUsername(), user.getEmail())) {
+            if(userService.checkEmailExists(user.getEmail())) {
+                model.addAttribute("emailExists", true);
+            } 
+            if(userService.checkUsernameExists(user.getUsername())) {
+                model.addAttribute("usernameExists", true);
+            }
+            return "signup";
+        } else {
+            Set<UserRole> userRoles = new HashSet<>();
+            userRoles.add(new UserRole(user, roleDao.findByName("USER")));
+            userService.createUser(user, userRoles);
+            
+            return "redirect:/";
+        }
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,1 +1,21 @@
 console.log('JAVASCRIPT IS WORKING');
+
+(function($){
+    $.toggleShowPassword = function(options) {
+        var settings = $.extend({
+            field:"#password",
+            control:'#showPassword',
+        }, options);
+        
+        var control = $(settings.control);
+        var field = $(settings.field);
+        
+        control.bind('click', function(){
+            if(control.is(':checked')) {
+                field.attr('type', 'text');
+            } else {
+                field.attr('type', 'password');
+            }
+        })
+    }
+})(jQuery);
